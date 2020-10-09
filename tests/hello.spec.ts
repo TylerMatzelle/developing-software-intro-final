c89293aa41e27b8dd16b058cc94f74fcac86a03e
Tyler Matzelle <tmatzelle16560@alumni.focuscollege.com>
2020-10-09 16:46:45 -0700

    made tests, added interfaces.ts file and versioned the package

--- tests/hello.spec.ts
+++ tests/hello.spec.ts
@@ -1,12 +1,18 @@
-import { calculateHouseMaterials, getAllHouseMaterials } from "../src/index";
 import { expect } from "chai";
 import "mocha";
+import { calcHouseMaterials, getHouseMaterials } from "../src/calculator";
 
-/*
-describe("Yargs commands", () => {
+
+describe("calculator", () => {
     it("should return specified parameters", () => {
-        const result = hello();
-        expect(result).to.equal("Hello");
+        const result = calcHouseMaterials(8, 8, true, "tyler");
+        expect(result).to.equal('width, length, units, name');
     });
 });
-*/
+
+describe("Auditor", () => {
+    it("should return the name of customer", () => {
+        const result = getHouseMaterials("tyler");
+        expect(result).to.equal('name');
+    });
+});
